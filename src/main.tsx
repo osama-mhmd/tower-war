b79eb08fd696834ff19ba989b0561787fafa576e
osama-mhmd <docosmo010@gmail.com>
2024-12-09 11:36:55 +0200

    fix: build

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Toaster from "./components/toaster.tsx";
 import Wrapper from "./wrapper.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // <Toaster>
   <Wrapper>
     <App />
   </Wrapper>
-  // </Toaster>
 );
