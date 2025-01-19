5986b4ef17863b35722984b3a41bd9a169c7a3d5
Osama Mohammed <docosmo010@gmail.com>
2025-01-19 19:19:46 +0200

    fix: wrapper takes a lot in loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Wrapper from "./wrapper.tsx";
+// import Wrapper from "./wrapper.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <Wrapper>
-    <App />
-  </Wrapper>
+  // <Wrapper>
+  <App />
+  // </Wrapper>
 );
